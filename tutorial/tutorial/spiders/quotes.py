41031d7305fa3eaa007a49a790d114a27a411654
unknown <marcuscheng@qsq.com>
2021-01-15 18:21:12 +0800

    20210115geng

--- tutorial/tutorial/spiders/quotes.py
+++ tutorial/tutorial/spiders/quotes.py
@@ -7,9 +7,11 @@ class QuotesSpider(scrapy.Spider):
     # start_urls = ['https://ihsmarkit.com/info/chi/supplier-highlights.html#sup-high-20-11']
     # allowed_domains = ['www.traceoption.com']
     # start_urls = ['https://traceoption.com/']
-    allowed_domains = ['quotes.toscrape.com']
-    start_urls = ['https://quotes.toscrape.com/']
+    allowed_domains = ["quotes.toscrape.com"]
+    start_urls = ['http://quotes.toscrape.com/']
 
+    def start_requests(self):
+        yield scrapy.Request(url='http://httpbin.org/post', method='POST', callback=self.parse_post)
 
     # def start_requests(self):
     #     cookiesstr=" _ga=GA1.2.1962155546.1608722731; _gid=GA1.2.1809730775.1610007390"
@@ -31,7 +33,8 @@ class QuotesSpider(scrapy.Spider):
     #         yield "测试"
     def parse(self, response):
         quotes = response.css('.quote')
-        # print(response.body)
+        tt = response.css('*')
+        print(tt)
         for quote in quotes:
             item=QuoteItem()
             item['text'] = quote.css('.text::text').extract_first()
@@ -41,3 +44,5 @@ class QuotesSpider(scrapy.Spider):
         next = response.css('.pager .next a::attr(href)').extract_first()
         url = response.urljoin(next)
         yield scrapy.Request(url=url, callback=self.parse)
+
+
